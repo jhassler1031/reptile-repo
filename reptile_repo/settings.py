dc5db2a8f4b55d9a0dfbc822917d3cd0d45405a4
jhassler1031 <jhassler1031@gmail.com>
2018-07-16 10:46:37 -0400

    initial migration

--- reptile_repo/settings.py
+++ reptile_repo/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-AUTH_USER_MODEL = 'app.User'
+AUTH_USER_MODEL = 'reptile_app.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
