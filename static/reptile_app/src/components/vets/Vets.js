28a23665560ec2a005f52cb5124a89976d6fd156
jhassler1031 <jhassler1031@gmail.com>
2018-07-20 15:03:34 -0400

    displaying the search map for vets

--- static/reptile_app/src/components/vets/Vets.js
+++ static/reptile_app/src/components/vets/Vets.js
@@ -1,3 +1,5 @@
+/* global google */
+
 import React, { Component } from 'react';
 import LocationSearchForm from '../SearchForms/LocationSearchForm.js';
 import Vet from './Vet.js';
@@ -7,6 +9,8 @@ import './Vets.css';
 // Import the utlitity file and set baseURL to the data
 import file from '../../utility.js';
 const baseURL = file.baseURL;
+
+let GOOGLE_API_KEY = localStorage.getItem("GOOGLE_API_KEY");
 
 
 class Vets extends Component {
@@ -20,12 +24,33 @@ class Vets extends Component {
     }
 
     this._locationSearch = this._locationSearch.bind(this);
+    this._initMap = this._initMap.bind(this);
+  }
+
+  // Loads the script tag needed by google maps, then called the initMap function to create the map
+  _loadJS() {
+    //This gives the window access to the _initMap function
+    window.initMap = this._initMap;
+    // Creates the script element, sets the source, and then appends to the body.  The source called initmap
+    let script = document.createElement("script");
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_API_KEY}&callback=initMap`;
+    document.body.appendChild(script);
+  }
+
+  // Function to create the map using the latLong provided by the search 
+  _initMap() {
+    let self = this;
+    let map = new google.maps.Map(document.getElementById('map'), {
+      center: self.state.latLong,
+      zoom: 10
+    });
   }
 
   // This function is called by the search form, creates the url for the fetch,
   // and then sets state to the data returned by the felch.
   // This also receives latLong from the form in order to create the map.
   _locationSearch(searchParams, latLong) {
+
     let self = this;
     // State here needs latLong to display the map
     self.setState({latLong: latLong});
@@ -50,6 +75,9 @@ class Vets extends Component {
     .catch((error)=>{
       console.log("There was a problem: \n", error);
     });
+
+    // Function to load a JS script tag in the body needed by google maps
+    self._loadJS();
   }
 
   render() {
@@ -65,10 +93,13 @@ class Vets extends Component {
       <div className="vetsContainer container">
         <h1>This is the Vets section.</h1>
         <LocationSearchForm search={this._locationSearch}/>
-
+        {/* <div className="mapContainer"> */}
+          <div id="map"></div>
+        {/* </div> */}
         <div className="searchResults">
           {/* If statement here to display either no search results message, or the search results */}
           {this.state.searchResults.length > 0 ? $vets : this.state.message}
+          {/* <script id="googleScript"></script> */}
         </div>
       </div>
     );
