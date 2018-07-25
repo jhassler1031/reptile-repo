c9f8d088b1f70b2c7ac7891d4f7b7bce11247332
jhassler1031 <jhassler1031@gmail.com>
2018-07-25 18:27:27 -0400

    added more tests

--- reptile_app/tests/test_urls.py
+++ reptile_app/tests/test_urls.py
@@ -71,13 +71,14 @@ class VetViewsetTest(TestCase):
             'boarding_services': response.data['boarding_services']
         }
 
-        # Make sure the planet was created with the values we provided
+        # Make sure the vet was created with the values provided
         self.assertEquals(test_data, vet_data)
 
 # Store model testing ==========================================================
 class StoreViewsetTest(TestCase):
     def setUp(self):
         self.store = mommy.make(Store)
+        self.user = mommy.make(User)
 
     def test_store_list(self):
         # Make sure the rest framework url is configured
@@ -99,10 +100,45 @@ class StoreViewsetTest(TestCase):
         response = c.get(reverse('store-detail', kwargs={'pk': self.store.pk}), content_type='application/json')
         self.assertEquals(200, response.status_code)
 
+    def test_store_create(self):
+        # Functional test: get detail of store
+        c = APIClient()
+
+        store_data = {
+            'store_name': 'testing',
+            'raw_address': 'testing street',
+            'city': 'spartanburg',
+            'state': 'sc',
+            'zip_code': '29307',
+            'notes': "test notes"
+        }
+
+        # Post request should create record and return status 201
+        token = Token.objects.create(user=self.user)
+        c.credentials(HTTP_AUTHORIZATION="Token " + token.key)
+        response = c.post(reverse('store-list'), store_data, format='json')
+        self.assertEquals(201, response.status_code)
+
+
+        # Pull out what data I can use to compare.  Some data will be created and we
+        # will not know what it is.
+        test_data = {
+            'store_name': response.data['store_name'],
+            'raw_address': response.data['raw_address'],
+            'city': response.data['city'],
+            'state': response.data['state'],
+            'zip_code': response.data['zip_code'],
+            'notes': response.data['notes'],
+        }
+
+        # Make sure the store was created with the values provided
+        self.assertEquals(test_data, store_data)
+
 # Illness model testing ========================================================
 class IllnessViewsetTest(TestCase):
     def setUp(self):
         self.illness = mommy.make(Illness)
+        self.user = mommy.make(User)
 
     def test_illness_list(self):
         # Make sure the rest framework url is configured
@@ -123,6 +159,36 @@ class IllnessViewsetTest(TestCase):
         c = Client()
         response = c.get(reverse('illness-detail', kwargs={'pk': self.illness.pk}), content_type='application/json')
         self.assertEquals(200, response.status_code)
+
+    def test_illness_create(self):
+        # Functional test: get detail of illness
+        c = APIClient()
+
+        illness_data = {
+            'illness_name': 'testing',
+            'symptoms': 'testing symptoms',
+            'description': 'testing',
+            'species_affected': 'bearded dragons',
+        }
+
+        # Post request should create record and return status 201
+        token = Token.objects.create(user=self.user)
+        c.credentials(HTTP_AUTHORIZATION="Token " + token.key)
+        response = c.post(reverse('illness-list'), illness_data, format='json')
+        self.assertEquals(201, response.status_code)
+
+
+        # Pull out what data I can use to compare.  Some data will be created and we
+        # will not know what it is.
+        test_data = {
+            'illness_name': response.data['illness_name'],
+            'symptoms': response.data['symptoms'],
+            'description': response.data['description'],
+            'species_affected': response.data['species_affected'],
+        }
+
+        # Make sure the illness was created with the values provided
+        self.assertEquals(test_data, illness_data)
 
 # Message Model Testing ========================================================
 class MessageViewsetTest(TestCase):
@@ -148,3 +214,31 @@ class MessageViewsetTest(TestCase):
         c = Client()
         response = c.get(reverse('message-detail', kwargs={'pk': self.message.pk}), content_type='application/json')
         self.assertEquals(200, response.status_code)
+
+    def test_message_create(self):
+        # Functional test: get detail of illness
+        c = APIClient()
+
+        message_data = {
+            'contact_name': 'testing',
+            'contact_email': 'testing symptoms',
+            'contact_phone': 'testing',
+            'message_text': 'test text',
+        }
+
+        # Post request should create record and return status 201
+        response = c.post(reverse('message-list'), message_data, format='json')
+        self.assertEquals(201, response.status_code)
+
+
+        # Pull out what data I can use to compare.  Some data will be created and we
+        # will not know what it is.
+        test_data = {
+            'contact_name': response.data['contact_name'],
+            'contact_email': response.data['contact_email'],
+            'contact_phone': response.data['contact_phone'],
+            'message_text': response.data['message_text'],
+        }
+
+        # Make sure the message was created with the values provided
+        self.assertEquals(test_data, message_data)
