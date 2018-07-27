671f6dd214d2f1b021474046efc63a10581b20c4
jhassler1031 <jhassler1031@gmail.com>
2018-07-27 17:52:46 -0400

    added User to admin site

--- reptile_app/admin.py
+++ reptile_app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from reptile_app.models import Vet, Store, Illness, Message
+from reptile_app.models import Vet, Store, Illness, Message, User
 
 # Register your models here.
+admin.site.register(User)
 admin.site.register(Vet)
 admin.site.register(Store)
 admin.site.register(Illness)
