f5b7dde535458d29bceec6842c76f886f29fa262
jhassler1031 <jhassler1031@gmail.com>
2018-07-31 10:50:47 -0400

    made changes to utility.js file

--- static/reptile_app/src/utility.js
+++ static/reptile_app/src/utility.js
@@ -1,13 +1,13 @@
-// const baseURL = "http://localhost:8000";
 const baseURL = "https://salty-earth-93578.herokuapp.com/";
+// const baseURL = "http://localhost:8000";
 
 const geoCodeURL = "https://maps.googleapis.com/maps/api/geocode/json";
 
+const loginURL = "https://salty-earth-93578.herokuapp.com/auth/token/create/";
 // const loginURL = "http://localhost:8000/auth/token/create/";
-const loginURL = "https://salty-earth-93578.herokuapp.com/auth/token/create/"
 
+const logoutURL = "https://salty-earth-93578.herokuapp.com/auth/token/destroy/";
 // const logoutURL = "http://localhost:8000/auth/token/destroy/";
-const logoutURL = "https://salty-earth-93578.herokuapp.com/auth/token/destroy/"
 
 // Google API Key locked down by Heroku URL
 const GOOGLE_API_KEY = "AIzaSyCoHbaK0VRre44HiamkKE6I9HU09pV-Cto";
